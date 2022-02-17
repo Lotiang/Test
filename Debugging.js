9cc44ea21c8a4b5f2af0573584000498c0510093
Lotiang Nyathio <93643349+Lotiang@users.noreply.github.com>
2022-02-17 12:31:01 +0300

    :art: update.Test

---
+++ Debugging.js
@@ -0,0 +1,28 @@
+//  Types of Debugging stages
+
+// parsing stage
+// is to varify syntax 
+
+// Excution stage
+// refer to process of calculation and prompting syntax
+
+// Types of Errors
+
+// syntax errors
+// miswriting the content
+
+// referrence error(underfined) and you define it by adding it as A String('') or array[].
+
+
+// logic error
+// it display wrong output bt it runs
+
+
+// runtime error
+// spelling errors cause by omission of some letters or miswriting the code
+
+// TypeError
+// Using wrong syntax, number of arguments
+
+// throw error
+// ('something bad happened!'); 
---
+++ myself.js
@@ -0,0 +1,9 @@
+//Declaration of my self
+let course = ''
+
+let mySelf1 = 'frontend';
+let mySelf2 = 'fullstack';
+let mySelf3 = 'backend';
+
+
+console.log(readVeryHard('mySelf1' + 'mySelf2')+ 'mySelf3');
