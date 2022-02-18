ce0df433f7d045fcaa0fd2e1b39342bac205b30b
Lotiang Nyathio <93643349+Lotiang@users.noreply.github.com>
2022-02-18 12:39:24 +0300

    :art: update.Test

---
+++ Arrays/arrowFunction.js
@@ -0,0 +1,22 @@
+let myName = () => {
+    console.log("Lotiang Nyathio");
+}
+myName();
+
+let power = (number) => {
+let result = number * number
+return result
+}
+console.log(power(6));
+
+let myName1 = (firstName, lastName) => {
+    let fullname = firstName + " " + lastName
+    return fullname
+}
+console.log(myName1('Lotiang', 'Nyathion'));
+
+let num = (num1, num2, num3) =>{
+    let ans = num1 * num2 * num3
+    return ans
+}
+console.log(num(2, 3, 4));
--- arrays,method.js
+++ arrays,method.js
@@ -23,6 +23,12 @@
 // popArray
 // shiftArray
 // unshiftArray
+// filterArray
+// mapArray
+// everyArray
+// someArray
+// findArray
+// reduceArray
 // let catfamily
 let catFamily = ['Lion', 'Leopard', 'Cat']
 // lenth = catFamily
@@ -124,3 +130,237 @@ document.getElementById("demo").innerHTML = fruits.includes("Mango");
 
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 document.getElementById("demo").innerHTML = fruits.includes("Banana", 3);
+
+// filterArray()method
+// Definition and Usage
+// The filter() method creates a new array filled with elements that pass a test provided by a function.
+
+// The filter() method does not execute the function for empty elements.
+
+// The filter() method does not change the original array.
+
+// See Also:
+// h1>JavaScript Arrays</h1>
+// <h2>The filter() Method</h2>
+
+// <p>Get every element in the array that has a value of 18 or more:</p>
+
+// <p id="demo"></p>
+
+// <script>
+// const ages = [32, 33, 16, 40];
+
+// document.getElementById("demo").innerHTML = ages.filter(checkAdult);
+
+// function checkAdult(age) {
+//   return age >= 18;
+
+// example
+const ages = [32, 33, 16, 40];
+const result = ages.filter(checkAdult);
+
+function checkAdult(age) {
+  return age >= 18;
+}
+console.log(result);
+
+// mapArray()method
+// Definition and Usage
+// map() creates a new array from calling a function for every array element.
+
+// map() calls a function once for each element in an array.
+
+// map() does not execute the function for empty elements.
+
+// map() does not change the original array.
+
+/* <h2>JavaScript Arrays</h2>
+
+<p>Multiply every element in the array with 10:</p>
+
+<p id="demo"></p>
+
+<script>
+const numbers = [65, 44, 12, 4];
+const newArr = numbers.map(myFunction);
+
+document.getElementById("demo").innerHTML = newArr;
+
+function myFunction(num) {
+  return num * 10;
+} */
+
+// example of mapArray
+<script>
+const numbers = [4, 9, 16, 25];
+document.getElementById("demo").innerHTML = numbers.map(Math.sqrt);
+</script>
+// results The Array.map() method creates a new array from the results of calling a function for every element.
+
+2,3,4,5
+
+const numbers = [65, 44, 12, 4];
+const newArr = numbers.map(myFunction)
+
+function myFunction(num) {
+  return num * 10;
+}
+console.log(newArr);
+
+// everyArray()Method
+// Definition and Usage
+// The every() method executes a function for each array element.
+
+// The every() method returns true if the function returns true for all elements.
+
+// The every() method returns false if the function returns false for one element.
+
+// The every() method does not execute the function for empty elements.
+
+// The every() method does not change the original array
+
+{/* <h1>JavaScript Arrays</h1>
+<h2>The every() Method</h2>
+
+<p>every() returns true if all elements in an array pass a test (provided as a function).</p>
+
+<p id="demo"></p>
+
+<script>
+const ages = [32, 33, 16, 40];
+document.getElementById("demo").innerHTML = ages.every(checkAge);
+
+function checkAge(age) {
+  return age > 18;
+  every() returns true if all elements in an array pass a test (provided as a function).
+answer;
+false
+}
+</script> */}
+// exaple of code
+const ages = [32, 33, 16, 40];
+
+ages.every(checkAge)
+
+function checkAge(age) {
+  return age > 18;
+}
+console.log(checkAge(ages));
+
+// someArray()method
+// Definition and Usage
+// The some() method checks if any array elements pass a test (provided as a function).
+
+// The some() method executes the function once for each array element:
+
+// If the function returns true, some() returns true and stops.
+// If the function returns false, some() returns false and stops.
+// The some() method does not execute the function for empty array elements.
+
+// The some() method does not change the original array.
+
+// <h2>JavaScript Arrays</h2>
+
+// <p>The Array.some() method checks if any of the elements in an array pass a test (provided as a function).</p>
+
+// <p id="demo"></p>
+
+// <script>
+// const ages = [3, 10, 18, 20];
+// document.getElementById("demo").innerHTML = ages.some(checkAdult);
+
+// function checkAdult(age) {
+//   return age > 18;
+// }
+// exampe of code
+const ages = [3, 10, 18, 20];
+
+ages.some(checkAdult);
+function checkAdult(age) {
+  return age > 18;
+}
+
+// findArray()method
+// Definition and Usage
+// The find() method returns the value of the first element that passes a test.
+
+// The find() method executes a function for each array element.
+
+// The find() method retuns undefined if no elements are found.
+
+// The find() method does not execute the function for empty elements.
+
+// The find() method does not change the original array.
+{/* <h1>JavaScript Arrays</h1>
+<h2>The find() Method</h2>
+
+<p>find() returns the value of the first element in an array that passes a test (provided by a function):</p>
+
+<p id="demo"></p>
+
+<script>
+const ages = [3, 10, 18, 20];
+
+document.getElementById("demo").innerHTML = ages.find(checkAge);
+
+function checkAge(age) {
+  return age > 18;
+}
+find() returns the value of the first element in an array that passes a test (provided by a function):
+
+20
+
+The find() method is not supported in Internet Explorer 11 (or earlier).
+</script>
+
+<p>The find() method is not supported in Internet Explorer 11 (or earlier).</p> */}
+// example of code
+const ages = [3, 10, 18, 20];
+
+function checkAge(age) {
+  return age > 18;
+}
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = ages.find(checkAge);
+}
+
+
+// reduceArray()method
+// Definition and Usage
+// The reduce() method executes a reducer function for array element.
+
+// The reduce() method returns a single value: the function's accumulated result.
+
+// The reduce() method does not execute the function for empty array elements.
+
+// The reduce() method does not change the original array.
+{/* <h2>JavaScript Arrays</h2>
+
+<p>Compute the sum of the rounded numbers in an array.</p>
+
+<p id="demo"></p>
+
+<script>
+const numbers = [15.5, 2.3, 1.1, 4.7];
+
+document.getElementById("demo").innerHTML = numbers.reduce(getSum, 0);
+
+function getSum(total, num) {
+  return total + Math.round(num);
+} */}
+// Compute the sum of the rounded numbers in an array.
+
+// 24
+
+// example of code
+const numbers = [15.5, 2.3, 1.1, 4.7];
+document.getElementById("demo").innerHTML = numbers.reduce(getSum, 0);
+
+function getSum(total, num) {
+  return total + Math.round(num);
+}
+
+
+
+
