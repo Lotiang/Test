6768aa9d18440372ea29ec08540e7a3f83a5a06a
Lotiang Nyathio <93643349+Lotiang@users.noreply.github.com>
2022-02-18 16:16:04 +0300

    :art: update.Test.js

--- SCORE-APP/yess.js
+++ SCORE-APP/yess.js
@@ -1,18 +1,39 @@
-alert('SCORE_,APP')
+// Manchester()
+function GoalM(){
+    var Manchester1=document.getElementById("goal1")
+    NewScore=Manchester1.innerHTML
+    NewScore++;
+    document.getElementById("goal1").innerHTML=NewScore
+}
 
-function ManchesterGoal_foul(){ 
-    let goal = document.getElementById('goal').innerHTML
-    let foul = document.getElementById('foul').innerHTML
-    goal++
-    foul++
-    document.getElementById("goal").innerText=goal
-    }  
+function FoulM(){
+    var Manchester1=document.getElementById("foul1")
+    NewScore=Manchester1.innerHTML
+    NewScore++;
+    document.getElementById("foul1").innerHTML=NewScore
+}
 
-    
-function ArsenalGoal_foul(){
-    let goal = document.getElementById('goal').innerHTML
-    let foul = document.getElementById('foul').innerHTML
-    goal++
-    foul++
-    document.getElementById("goal").innerText=goal
-    }  
+// Arsenal
+function GoalA(){
+    var Arsenal2=document.getElementById("goal2")
+    NewScore=Arsenal2.innerHTML
+    NewScore++;
+    document.getElementById("goal2").innerHTML=NewScore
+}
+
+function FoulA(){
+    var Arsenal2=document.getElementById("foul2")
+    NewScore=Arsenal2.innerHTML
+    NewScore++;
+    document.getElementById("foul2").innerHTML=NewScore
+}
+
+
+// Reset
+function Reset(){
+    var reset3=document.getElementById("reset3")
+    NewScore=reset3.innerHTML
+    NewScore=0;
+    document.getElementById("goal1",  "goal2").innerHTML=NewScore
+}
+
