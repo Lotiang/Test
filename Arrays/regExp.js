a1bcffdec79e03db6e99c30466c4c9cbbbafa6ea
Lotiang Nyathio <93643349+Lotiang@users.noreply.github.com>
2022-03-08 12:33:31 +0300

    :+1: update.swtch.js

---
+++ Arrays/regExp.js
@@ -0,0 +1,41 @@
+// JavaScript Regular Expressions
+// A regular expression is a sequence of characters that forms a search pattern.
+
+// The search pattern can be used for text search and text replace operations.
+
+// What Is a Regular Expression?
+// A regular expression is a sequence of characters that forms a search pattern.
+
+// When you search for data in a text, you can use this search pattern to describe what you are searching for.
+
+// A regular expression can be a single character, or a more complicated pattern.
+
+// Regular expressions can be used to perform all types of text search and text replace operations.
+
+// Example
+// Using String Methods
+// In JavaScript, regular expressions are often used with the two string methods: search() and replace().
+
+// The search() method uses an expression to search for a match, and returns the position of the match.
+
+// The replace() method returns a modified string where the pattern is replaced.
+
+// Using String search() With a String
+// The search() method searches a string for a specified value and returns the position of the match:
+
+// Example
+// Use a string to do a search for "W3schools" in a string:
+
+// let text = "Visit W3Schools!";
+// let n = text.search("W3Schools");
+// The result in n will be:
+
+6
+// Example explained:
+
+// /w3schools/i  is a regular expression.
+
+// w3schools  is a pattern (to be used in a search).
+
+// i  is a modifier (modifies the search to be case-insensitive).
+
